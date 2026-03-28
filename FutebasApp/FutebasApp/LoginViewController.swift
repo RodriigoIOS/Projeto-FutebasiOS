ce5b3c33c62a8d1290cb18692799cd06875de422
Rodrigo Garcia <zfirelord@Rodrigos-iMac.local>
2026-03-28 09:49:29 -0300

    Adicionando estrutura de pastas para o projeto inicial

--- FutebasApp/FutebasApp/LoginViewController.swift
+++ FutebasApp/FutebasApp/LoginViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    var loginLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Login"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "System", size: 12)
+        return label
+    }()
+    
+    var passwordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Senha"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "System", size: 12)
+        return label
+    }()
+    
+    var loginButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBlue
+        
+        return button
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemMint
+        view.backgroundColor = .systemGreen
+        addSubviews()
+        addConstraints()
+    }
+    
+    @objc func actionLoginBtn() {
+        
+    }
+    
+    func addSubviews() {
+        
+    }
+    
+    func addConstraints() {
+        
     }
 }
